8a5ec7360f46eecf5a714ecc04773879829d72f2
Artur Fujarski <artur.fujarski@capgemini.com>
2021-09-06 15:29:56 +0200

    adjusted IP for nested internet

--- windows/main.tf
+++ windows/main.tf
@@ -57,16 +57,23 @@ resource "azurerm_network_security_rule" "example3" {
 
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.2.0.0/16"]
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 }
 
 resource "azurerm_subnet" "internal" {
-  name                 = "internal"
+  name                 = "nat"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = ["10.2.0.0/24"]
+}
+
+resource "azurerm_subnet" "lan" {
+  name                 = "lan"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.2.1.0/24"]
 }
 
 resource "azurerm_public_ip" "pip" {
